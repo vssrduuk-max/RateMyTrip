fd0796d1a538f3c3aeef0887de6cf6138ff25c65
vssrduuk-max <vssrduuk@gmail.com>
2026-04-22 16:37:36 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'travel-mvp-v1'
+const SHELL = ['./', './manifest.webmanifest']
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)))
+  self.skipWaiting()
+})
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))))
+  self.clients.claim()
+})
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+})
